d0b67232850e01780ec06efaeed19b96660bf392
rizk19 <rizkiahf@gmail.com>
2023-01-06 11:22:20 +0700

    mailjet config

--- api-lib/mailJet.js
+++ api-lib/mailJet.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
   // TODO: Replace with the email you want to use to send email
   from: process.env.MAILJET_SENDER_MAIL,
-  name: 'Developer Rayandra',
+  name: 'Developer Rafsha',
 };
