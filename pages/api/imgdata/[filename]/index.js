bafe5cde7a614966127517036887c88e22c7803b
rizk19 <rizkiahf@gmail.com>
2023-01-06 11:23:41 +0700

    handling delete img

--- pages/api/imgdata/[filename]/index.js
+++ pages/api/imgdata/[filename]/index.js
@@ -1,4 +1,4 @@
-import { findPhotosByFileName } from '@/api-lib/db';
+import { findPhotosByFileName, findAndDeletePhotosByName } from '@/api-lib/db';
 import { getMongoDb } from '@/api-lib/mongodb';
 import { ncOpts } from '@/api-lib/nc';
 import nc from 'next-connect';
@@ -9,9 +9,22 @@ handler.get(async (req, res) => {
   const db = await getMongoDb();
   const img = await findPhotosByFileName(db, req.query.filename);
   let data = { ...img };
+  if (data && Object.keys(data) && Object.keys(data).length > 0) {
+    data.url = `${process.env.WEB_URI}api/img/${data.filename}`;
+    data.dataurl = `${process.env.WEB_URI}api/imgdata/${data.filename}`;
+    res.json({ img: data });
+  } else {
+    res.json({ data: 'Data not found' });
+  }
+});
+
+handler.delete(async (req, res) => {
+  const db = await getMongoDb();
+  const imgDelete = await findAndDeletePhotosByName(db, req.query.filename);
+  let data = { ...imgDelete };
   data.url = `${process.env.WEB_URI}api/img/${data.filename}`;
   data.dataurl = `${process.env.WEB_URI}api/imgdata/${data.filename}`;
-  res.json({ img: data });
+  res.json({ message: 'Successfully deleted', img: data });
 });
 
 export default handler;
